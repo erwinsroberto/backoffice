201e29e20391a422f9a0abce90c8eec52ad495e4
Erwins Roberto <erwinsroberto@gmail.com>
2023-06-05 19:11:54 -0300

    arrumei o espaçamento

--- cypress/e2e/cardapio.cy.js
+++ cypress/e2e/cardapio.cy.js
@@ -1,6 +1,4 @@
 //esses códigos rodam em sequência semelhante a um passo a passo e as ações são de acordo com o nome dos comandos
-
-
 describe('faz login no backoffice e cria, edita, altera e exclui um cardápio', () => {
     it('testes do cardápio', () => {
         cy.login();
